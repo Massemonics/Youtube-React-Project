5a80a3ed894a54d081f470d42173955a38303bce
Daniel Naranjo <MR.DANIEL.NARANJO@PROTONMAIL.COM>
2021-05-19 16:57:51 -0400

    pair programming

--- src/Components/About.js
+++ src/Components/About.js
@@ -2,22 +2,19 @@ import './About.css'
 const About = () => {
     return (
         <div className="about">
-            <p>About</p>
+             <p>A YouTube mock-up that uses the YouTube API, to renders videos and allows the user post comments</p>
             <div>
-            <p>A Brief Project Description</p>
-            <p>A Short Bio of Daniel Naranjo</p>
-            <a href="https://github.com/PompaDonpa">Visit Daneil's GitHub </a>
+                <p>A Short Bio of Daniel Naranjo</p>
+                <a href="https://github.com/PompaDonpa">Visit Daniel's GitHub </a>
             </div>
 
             <div>
-            <p>A Brief Project Description</p>
             <p>A Short Bio of Du Min Chen </p>
             <a href="https://github.com/Dumin1120">Visit Dumins's GitHub </a>
             </div>
 
 
             <div>
-            <p>A Brief Project Description</p>
             <p>A Short Bio of Quisa Hines </p>
             <a href="https://github.com/Dequisa">Visit Quisa's GitHub </a>
             </div>
