cb7666039f9ea54157ce1553a269d96d7a20ed01
Daniel Naranjo <75263240+PompaDonpa@users.noreply.github.com>
2021-05-18 20:07:43 -0400

    Update NavBar.js

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -8,8 +8,6 @@ const NavBar = () => {
         <div className="navbar">
         <nav>
             <p>Youtube</p>  
-                {/* I've deleted the {" "}, because has no effect when using CSS, 
-                and it's a better practice (I know because I learned from Colin's Code review) */}
             <Link to="/">Home</Link>
             <Link to="/about">About</Link>
         </nav>
