ebf74fee28e0e7ab30d104a5258b53d739b86bc9
Daniel Naranjo <MR.DANIEL.NARANJO@PROTONMAIL.COM>
2021-05-18 20:10:58 -0400

    Issue at merge

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,19 +1,16 @@
 import './NavBar.css'
-import React from "react";
-import { Link } from 'react-router-dom';
-
+import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NavBar = () => {
-    return(
-        <div className="navbar">
-        <nav>
-            <p>Youtube</p>  
-                {/* I've deleted the {" "}, because has no effect when using CSS, 
-                and it's a better practice (I know because I learned from Colin's Code review) */}
-            <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
-        </nav>
-        </div>
-    );
-};
-export default NavBar;
+  return (
+    <div className='navbar'>
+      <nav>
+        <p>Youtube</p>
+        <Link to='/'>Home</Link>
+        <Link to='/about'>About</Link>
+      </nav>
+    </div>
+  )
+}
+export default NavBar
