9eabf8b3ec297948cffbcbc72e48adcfac58e07e
Daniel Naranjo <MR.DANIEL.NARANJO@PROTONMAIL.COM>
2021-05-20 14:59:14 -0400

    Daniel's bio added

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,9 +3,21 @@ const About = () => {
     return (
         <div className="about">
              <p>A YouTube mock-up that uses the YouTube API, to renders videos and allows the user post comments</p>
-            <div>
-                <p>A Short Bio of Daniel Naranjo</p>
-                <a href="https://github.com/PompaDonpa">Visit Daniel's GitHub </a>
+            <div className="daniel-card">
+                <p>- A Short Bio of <strong>Daniel Naranjo</strong></p>
+                <div>
+                    <ul>
+                        <li>I'm originally from Ecuador, moved to USA in 2015</li>
+                        <li>I've joined Pursuit to be part of the 3.0 web revolution of disruptive 
+                            technologies like Artifical Intelligence and the block chain.</li>
+                        <li>A fun fact about me is that I like Philosophy, Bodybuilding, Economics and Trading</li>
+                    </ul>
+                    <p>
+                        <a href="https://github.com/PompaDonpa"><img src="https://api.iconify.design/eva:github-fill.svg" alt="octocat" /></a>
+                        Visit Daniel's<a href="https://github.com/PompaDonpa">GitHub</a>
+                    </p>
+                </div>
+
             </div>
 
             <div>
