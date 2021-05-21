34e11b2545a3fe7282dc3cd1604b206134bfa8a4
Du Min Chen <dm.chen0113@gmail.com>
2021-05-21 12:08:50 -0400

    Add load more button, my short bio, and some clean up

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,14 +1,20 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import axios from "axios";
-import moment from 'moment';
-import 'moment-duration-format'
-import "./Home.css"
+import moment from "moment";
+import "moment-duration-format";
+import "./Home.css";
 
 export default class Home extends Component {
     constructor() {
         super();
-        this.state = { userSearch: "", searchResult: [], videosStats: [], isSearch: false };
+        this.state = {
+            searchInput: "",
+            currentSearch: "",
+            searchResult: {},
+            videosDetails: [],
+            isSearch: false,
+        }
     }
 
     formatDuration = (time) => {
@@ -17,34 +23,61 @@ export default class Home extends Component {
     }
 
     handleUserInput = (e) => {
-        this.setState({ userSearch: e.target.value });
+        this.setState({ searchInput: e.target.value });
     }
     submitUserSearch = async (e) => {
         e.preventDefault();
-        const { data: { items: searchResult } } = await axios.get(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${this.state.userSearch}&type=video&key=${process.env.REACT_APP_API_KEY}`);
-        const videosIds = searchResult.map(video => video.id.videoId).join(",");
-        const { data: { items: videosStats } } = await axios.get(`https://www.googleapis.com/youtube/v3/videos?id=${videosIds}&part=snippet,contentDetails,statistics&key=${process.env.REACT_APP_API_KEY}`);
-        this.setState({ userSearch: "", searchResult, videosStats, isSearch: true });
+        const searchInput = this.state.searchInput.trim();
+        if (!searchInput) return;
+        const { data: searchResult } = await axios.get(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${searchInput}&type=video&key=${process.env.REACT_APP_API_KEY}`);
+        const videosIds = searchResult.items.map(video => video.id.videoId).join(",");
+        const { data: { items: videosDetails } } = await axios.get(`https://www.googleapis.com/youtube/v3/videos?id=${videosIds}&part=snippet,contentDetails,statistics&key=${process.env.REACT_APP_API_KEY}`);
+        this.setState({
+            searchInput: "",
+            currentSearch: searchInput,
+            searchResult,
+            videosDetails,
+            isSearch: true,
+        })
+    }
+    loadMoreVideos = async () => {
+        const { currentSearch, searchResult: { nextPageToken: token } } = this.state;
+        const { data: searchResult } = await axios.get(`https://youtube.googleapis.com/youtube/v3/search?pageToken=${token}&part=snippet&maxResults=25&q=${currentSearch}&type=video&key=${process.env.REACT_APP_API_KEY}`);
+        const videosIds = searchResult.items.map(video => video.id.videoId).join(",");
+        const { data: { items: videosDetails } } = await axios.get(`https://www.googleapis.com/youtube/v3/videos?id=${videosIds}&part=snippet,contentDetails,statistics&key=${process.env.REACT_APP_API_KEY}`);
+        this.setState({
+            searchResult,
+            videosDetails: [...this.state.videosDetails, ...videosDetails]
+        })
     }
 
     render() {
-        const { userSearch, videosStats, isSearch } = this.state;
-        const videosList = videosStats.map(list => {
-            const duration = this.formatDuration(list.contentDetails.duration)
+        const { searchInput, searchResult, videosDetails, isSearch } = this.state;
+        const videosList = videosDetails.map(list => {
+            const duration = this.formatDuration(list.contentDetails.duration);
             return (
                 <li key={list.id} className="single-video">
                     <div className="video-img">
                         <Link to={`/videos/${list.id}`}>
                             <img src={list.snippet.thumbnails.medium.url} alt="" />
-                        <span className={duration === "LIVE" ? "stat-duration-live" : "stat-duration"}>{duration}</span>
+                            <span className={duration === "LIVE" ? "stat-duration-live" : "stat-duration"}>
+                                {duration}
+                            </span>
                         </Link>
                     </div>
-                    <div className='video-card'>
+                    <div className="video-card">
                         <Link to={`/videos/${list.id}`}>
                             <span className="video-title">{list.snippet.title}</span>
                         </Link>
-                        <p className="stat">Views: {parseInt(list.statistics.viewCount).toLocaleString()}</p>
-                        <p><img src="https://api.iconify.design/bx:bxs-like.svg" alt="" /> {list.statistics.likeCount || 0} <img src="https://api.iconify.design/bx:bxs-dislike.svg" alt="" /> {list.statistics.dislikeCount || 0}</p>
+                        <p className="stat">
+                            Views: {parseInt(list.statistics.viewCount).toLocaleString()}
+                        </p>
+                        <p>
+                            <img src="https://api.iconify.design/bx:bxs-like.svg" alt="" />
+                            {parseInt(list.statistics.likeCount || 0).toLocaleString()}&nbsp;&nbsp;&nbsp;
+                            <img src="https://api.iconify.design/bx:bxs-dislike.svg" alt="" />
+                            {parseInt(list.statistics.dislikeCount || 0).toLocaleString()}
+                        </p>
                     </div>
                 </li>
             )
@@ -58,7 +91,7 @@ export default class Home extends Component {
                         type="text"
                         placeholder="Search for videos"
                         onChange={this.handleUserInput}
-                        value={userSearch}
+                        value={searchInput}
                     />
                     <input
                         type="submit"
@@ -66,6 +99,7 @@ export default class Home extends Component {
                     />
                 </form>
                 {isSearch && displayInfo}
+                {searchResult.nextPageToken && <button className="load-more" onClick={this.loadMoreVideos}>Load more...</button>}
             </div>
         )
     }
